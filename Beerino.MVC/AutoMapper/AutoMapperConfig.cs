f0f555fe466927142f1d958833b21600448f48ac
Marcos Michel Machado Koch <marcos.koch@cwi.com.br>
2016-10-27 17:16:41 -0200

    AutoMapperConfig

--- Beerino.MVC/AutoMapper/AutoMapperConfig.cs
+++ Beerino.MVC/AutoMapper/AutoMapperConfig.cs
@@ -1,6 +1,16 @@
-﻿namespace Beerino.MVC.AutoMapper
+﻿using AutoMapper;
+
+namespace Beerino.MVC.AutoMapper
 {
     public class AutoMapperConfig
     {
+        public static void RegisterMappings()
+        {
+            Mapper.Initialize(x =>
+            {
+                x.AddProfile<DomainToViewModelMappingProfile>();
+                x.AddProfile<ViewModelToDomainMappingProfile>();
+            });
+        }
     }
 }
